8ac8c71bc4ecbcd39a2317d2d7f156e646e9b107
sanderman01 <sanderman@gmail.com>
2014-01-25 11:49:39 +0100

    Triggers and Actions
    Just a start. Not finished yet.

---
+++ Programming/particles-proof-of-concept/Assets/Scripts/Triggering/Action.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Action : MonoBehaviour
+{
+	public virtual void TriggerAction()
+	{
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, 0.1f);
+	}
+}
---
+++ Programming/particles-proof-of-concept/Assets/Scripts/Triggering/Trigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour
+{
+		public Action[] actions = new Action[0];
+
+		void Reset ()
+		{
+				if (collider != null)
+						collider.isTrigger = true;
+		}
+
+		void OnTriggerEnter (Collider collider)
+		{
+				// trigger action
+		}
+
+		void OnDrawGizmos ()
+		{
+				Gizmos.color = Color.green;
+				Gizmos.DrawWireSphere (transform.position, 0.1f);
+				for (int i = 0; i < actions.Length; ++i) {
+						Action a = actions [i];
+						if (a != null) {
+								Gizmos.DrawLine (transform.position, a.transform.position);
+						}
+				}
+		}
+}
