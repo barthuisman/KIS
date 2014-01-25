d779c66c5b6addcb7b19e00a9fb45acbc358a73a
sanderman01 <sanderman@gmail.com>
2014-01-25 23:09:16 +0100

    Add ActionSetEnabled

---
+++ Programming/particles-proof-of-concept/Assets/Scripts/Triggering/ActionSetEnabled.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionSetEnabled : Action
+{
+		public MonoBehaviour targetMonoBehaviour;
+		public enum Mode
+		{
+				Activate,
+				Deactivate,
+				Toggle
+		}
+		public Mode mode;
+
+		public override void TriggerAction ()
+		{
+				if (targetMonoBehaviour != null) {
+						switch (mode) {
+						case Mode.Activate:
+								targetMonoBehaviour.enabled = true;
+								break;
+						case Mode.Deactivate:
+								targetMonoBehaviour.enabled = false;
+								break;
+						case Mode.Toggle:
+								targetMonoBehaviour.enabled = !targetMonoBehaviour.enabled;
+								break;
+						}
+				}
+		}
+}
