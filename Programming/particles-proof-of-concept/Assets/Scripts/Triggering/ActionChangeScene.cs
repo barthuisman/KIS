c4ca06e9403999fcec017d89127567b02bc4583d
sanderman01 <sanderman@gmail.com>
2014-01-25 21:35:02 +0100

    Add ActionChangeScene
    Trigger Action used to change scenes.

---
+++ Programming/particles-proof-of-concept/Assets/Scripts/Triggering/ActionChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionChangeScene : Action
+{
+	public string sceneName;
+
+	public override void TriggerAction ()
+	{
+		if(string.IsNullOrEmpty(sceneName))
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+		else
+		{
+			Application.LoadLevel(sceneName);
+		}
+	}
+}
