e64f1f639e4c555378f7a94cff3ffe0af894ed39
Aldwoni <aldwoni@gmail.com>
2014-01-25 09:54:02 +0100

    Footprint version 0.1
    Footprint version 0.1

---
+++ Programming/particles-proof-of-concept/Assets/Scripts/FootPrint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootPrint : MonoBehaviour {
+	public GameObject footPrint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate(footPrint, gameObject.transform.position, new Quaternion(90,90,0,0));
+	}
+}
