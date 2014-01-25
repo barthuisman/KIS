0bf19560e506b4cda509b6324d03d3330215845c
Aldwoni <aldwoni@gmail.com>
2014-01-25 13:26:34 +0100

    FootPrints v0.7
    FootPrints v0.7

--- Programming/particles-proof-of-concept/Assets/Scripts/FootPrint.cs
+++ Programming/particles-proof-of-concept/Assets/Scripts/FootPrint.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FootPrint : MonoBehaviour {
-	public GameObject footPrint;
-	// Use this for initialization
-	void Start () {
+public class FootPrint : MonoBehaviour
+{
+		public GameObject footPrintLeft;
+		public GameObject footPrintRight;
+		public float seconds;
+		private bool rightFoot;
+		private Transform oldTransform;
+		private Vector3 oldPosition;
+	private float minDistance =0.5f;
+
+		void Start ()
+		{
+				oldTransform = transform.parent;
+				seconds = 0.1f;	
+				StartCoroutine (WaitSeconds());
+		}
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Instantiate(footPrint, gameObject.transform.position, new Quaternion(90,90,0,0));
-	}
+		IEnumerator WaitSeconds ()
+		{
+		Vector3 temp=  Vector3.zero;
+		Debug.Log(temp);
+				yield return new WaitForSeconds (seconds);
+				switch (rightFoot) {
+				case true:
+				temp = oldPosition - transform.parent.position;
+				
+				if(temp.sqrMagnitude > minDistance*minDistance){
+								Debug.Log(rightFoot);
+								rightFoot = false;
+								Instantiate (footPrintRight, transform.position, new Quaternion (0, transform.parent.rotation.y + 90, 0, 0));
+								oldPosition = transform.parent.position;
+						}
+						break;
+				case false:
+			temp = oldPosition - transform.parent.position;
+			if(temp.sqrMagnitude > minDistance*minDistance) {
+								rightFoot = true;
+								Instantiate (footPrintLeft, transform.position, new Quaternion (0, transform.parent.rotation.y + 90, 0, 0));
+								oldPosition = transform.parent.position;
+						}
+								//StartCoroutine (WaitSeconds ());
+						break;
+				}
+				StartCoroutine (WaitSeconds ());
+
+				
+		}
 }
