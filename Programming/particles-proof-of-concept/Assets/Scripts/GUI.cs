15a2dd8aa402bb0110a515091ab4a19a9984f99a
Aldwoni <aldwoni@gmail.com>
2014-01-25 15:03:04 +0100

    GUI part 1

---
+++ Programming/particles-proof-of-concept/Assets/Scripts/GUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F4))
+		{
+			Debug.Log(0);
+			Application.LoadLevel(0);
+		}
+	}
+}
