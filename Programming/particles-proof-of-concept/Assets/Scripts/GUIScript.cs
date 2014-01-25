87cf03c72faeaa3e768c1dbe7cddf7556ad37df0
Aldwoni <aldwoni@gmail.com>
2014-01-25 15:56:03 +0100

    2 changes
    added resume button
    f4 = f5

--- Programming/particles-proof-of-concept/Assets/Scripts/GUIScript.cs
+++ Programming/particles-proof-of-concept/Assets/Scripts/GUIScript.cs
@@ -5,7 +5,7 @@ public class GUIScript : MonoBehaviour {
 	public bool boolGUI = false;
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.F4))
+		if(Input.GetKeyDown(KeyCode.F5))
 		{
 			Application.LoadLevel(0);
 		}
@@ -18,9 +18,9 @@ public class GUIScript : MonoBehaviour {
 	{
 		if(boolGUI)
 		{
-			if(GUI.Button(new Rect(0,0,100,100), "Quit"))
+			if(GUI.Button(new Rect(0,0,100,100), "Resume"))
 			{
-				Application.Quit();
+				boolGUI = false;
 			}
 			if(GUI.Button(new Rect(0,100,100,100), "Start"))
 			{
