e1b35f3b6a522c673f6dd061a8a201ac6c04d85c
sanderman01 <sanderman@gmail.com>
2014-01-26 10:23:14 +0100

    Triggers and Audio.

---
+++ Programming/particles-proof-of-concept/Assets/Scripts/Triggering/ActionAccumulator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Triggers after being triggered X times.
+public class ActionAccumulator : Action
+{
+		public Action[] actions = new Action[0];
+		public int targetCount;
+		public int currentCount = 0;
+
+		public override void TriggerAction ()
+		{
+				currentCount++;
+				if (currentCount >= targetCount) {
+						currentCount = 0;
+						InternalTrigger ();
+				}
+		}
+
+		void InternalTrigger ()
+		{	
+				for (int i = 0; i < actions.Length; ++i) {
+						Action a = actions [i];
+						if (a != null) {
+								Debug.Log (string.Format ("ActionAccumulator {0} signalling Action {1}.", this.name, a.name), this);
+								a.TriggerAction ();
+						}
+				}
+		}
+
+		protected override void OnDrawGizmos ()
+		{
+				base.OnDrawGizmos();
+				Gizmos.color = Color.green;
+				for (int i = 0; i < actions.Length; ++i) {
+						Action a = actions [i];
+						if (a != null) {
+								Gizmos.DrawLine (transform.position, a.transform.position);
+						}
+				}
+		}
+}
