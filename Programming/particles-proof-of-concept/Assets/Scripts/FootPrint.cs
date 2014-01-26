fff742de6e37f3aebef3ce20802d49b833421c48
sanderman01 <sanderman@gmail.com>
2014-01-26 14:41:12 +0100

    Sounds and footsteps

--- Programming/particles-proof-of-concept/Assets/Scripts/FootPrint.cs
+++ Programming/particles-proof-of-concept/Assets/Scripts/FootPrint.cs
@@ -6,38 +6,47 @@ public class FootPrint : MonoBehaviour
 		public GameObject footPrintLeft;
 		public GameObject footPrintRight;
 		public float seconds;
+		public AudioClip[] footstepClips;
 		private bool rightFoot;
 		private Vector3 oldPosition;
-	private float minDistance =0.5f;
+		private float minDistance = 0.5f;
+
+		private System.Random rand = new System.Random();
 
 		void Start ()
 		{
 				seconds = 0.1f;	
-				StartCoroutine (WaitSeconds());
+				StartCoroutine (WaitSeconds ());
 		}
 	
 		IEnumerator WaitSeconds ()
 		{
-		Vector3 temp=  Vector3.zero;
+				Vector3 temp = Vector3.zero;
 				yield return new WaitForSeconds (seconds);
 				switch (rightFoot) {
 				case true:
-				temp = oldPosition - transform.parent.position;
+						temp = oldPosition - transform.parent.position;
 				
-				if(temp.sqrMagnitude > minDistance*minDistance){
+						if (temp.sqrMagnitude > minDistance * minDistance) {
 								rightFoot = false;
-								Instantiate (footPrintRight, transform.position, new Quaternion (0, (transform.parent.rotation.y + 90), 0, 0));
+								Quaternion orientation = Quaternion.LookRotation(transform.TransformDirection(Vector3.forward));
+								Instantiate (footPrintRight, transform.position, orientation);
 								oldPosition = transform.parent.position;
 								//Debug.Log(transform.parent.rotation.y);
+								audio.clip = footstepClips [rand.Next(footstepClips.Length-1)];
+								audio.Play ();
 						}
 						break;
 				case false:
-			temp = oldPosition - transform.parent.position;
-			if(temp.sqrMagnitude > minDistance*minDistance) {
+						temp = oldPosition - transform.parent.position;
+						if (temp.sqrMagnitude > minDistance * minDistance) {
 								rightFoot = true;
-								Instantiate (footPrintLeft, transform.position, new Quaternion (0, (transform.parent.rotation.y + 90), 0, 0));
+								Quaternion orientation = Quaternion.LookRotation(transform.TransformDirection(Vector3.forward));
+								Instantiate (footPrintLeft, transform.position, orientation);
 								oldPosition = transform.parent.position;
-				//Debug.Log(transform.parent.rotation.y);
+								//Debug.Log(transform.parent.rotation.y);
+								audio.clip = footstepClips [rand.Next(footstepClips.Length-1)];
+								audio.Play ();
 						}
 						break;
 				}
--- Programming/particles-proof-of-concept/Assets/Scripts/Suicide.cs
+++ Programming/particles-proof-of-concept/Assets/Scripts/Suicide.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Suicide : MonoBehaviour {
-	public int seconds;
-	// Use this for initialization
-	void Start () {
-		seconds=5;
-		StartCoroutine(DestroyAfterSeconds());
-	}
+public class Suicide : MonoBehaviour
+{
+		public int seconds;
+		// Use this for initialization
+		void Start ()
+		{
+				StartCoroutine (DestroyAfterSeconds ());
+		}
 	
-	IEnumerator DestroyAfterSeconds() {
-		//print(Time.time);
-		yield return new WaitForSeconds(seconds);
-		Destroy(this.gameObject);
-		//print(Time.time);
-	}
-	// Update is called once per frame
-	void Update () {
+		IEnumerator DestroyAfterSeconds ()
+		{
+				//print(Time.time);
+				yield return new WaitForSeconds (seconds);
+				Destroy (this.gameObject);
+				//print(Time.time);
+		}
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
+		}
 }
